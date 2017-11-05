ab7ff9d068c09fe3e7ee14bbd66049dab47da643
Aleksey Romanov <lex.at.net@gmail.com>
2017-11-05 12:22:35 +0600

    add second group file and refresh 5-7 files

---
+++ 2017/8-11/shapes.py
@@ -0,0 +1,57 @@
+def itemOutput(item):
+	color = 0;
+	dimentions = 1;
+	weight = 2;
+	print('shape color={} dimentions={} weight={}'.format(
+		item[color],
+		item[dimentions],
+		item[weight]
+		)
+	)
+
+def printBucket(bucketO, bucketName):
+	if (len(bucketO) == 0):
+		print('{} is empty'.format(bucketName))
+		return
+	print('In {}'.format(bucketName))
+	for i in range(0, len(bucketO)):
+		itemOutput(bucketO[i])
+	message = 'end of bucket {}'.format(bucketName)
+	print(message)
+	
+def analyseBucket(bucket, redBucket, blueBucket):
+	for i in range(0, len(bucket)):
+		item = bucket[i];
+		if (item[color] == 'red'):
+			redBucket.append(item)
+		if ((item[color] == 'blue') and (item[weight] < 2)):
+			blueBucket.append(item)
+
+bucket = [
+('red', 1, 1),
+('blue', 1, 1),
+('red', 2, 1),
+('red', 1, 4),
+('blue', 1, 2),
+('red', 1, 1)
+];
+
+printBucket(bucket, "BaseBucket")
+
+bucket1=[];
+bucket2=[];
+bucket3=[];
+bucket4=[];
+
+
+color = 0;
+dimentions = 1;
+weight = 2;
+
+
+
+analyseBucket(bucket, bucket1, bucket2)
+printBucket(bucket1, "Red bucket")
+printBucket(bucket2, "Blue lite bucket")
+printBucket(bucket3, "bucket3")
+printBucket(bucket4, "bucket4")
