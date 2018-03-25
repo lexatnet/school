dd5ee7bd6e6fc818d0fdf131319dd5f87a9f249c
Aleksey Romanov <lex.at.net@gmail.com>
2018-03-25 11:25:40 +0600

    fix module example for python 3.6

--- python/11 - modules/module1/__init__.py
+++ python/11 - modules/module1/__init__.py
@@ -1,2 +1,8 @@
+import os
+file_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(file_path)
+import sys
+sys.path.append(dir_path)
+
 from mo2 import some1, func1
 from func2 import func2
--- python/11 - modules/module1/mo2/__init__.py
+++ python/11 - modules/module1/mo2/__init__.py
@@ -1,3 +1,9 @@
+import os
+file_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(file_path)
+import sys
+sys.path.append(dir_path)
+
 from func1 import func1
 def some1():
     print('some1 is prints')
