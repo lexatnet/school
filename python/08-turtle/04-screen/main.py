f3d01a83eec00a98680b3e7883c644357bd4ac23
lexatnet <lex.at.net@gmail.com>
2018-12-03 23:10:12 +0600

    fix python turtle readme

--- python/08-turtle/04-screen/main.py
+++ python/08-turtle/04-screen/main.py
@@ -1,1 +1,15 @@
- 
+from turtle import Turtle, Screen
+
+screen = Screen()
+
+screen.bgcolor('green')
+screen.screensize(100, 100)
+
+screen_width, screen_height = screen.screensize()
+screen.setworldcoordinates(0, 0, screen_width, screen_height)
+
+t = Turtle()
+
+t.goto(screen_width//2, screen_height//2)
+
+screen.exitonclick()
